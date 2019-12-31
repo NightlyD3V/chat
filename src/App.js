7b63f3338dffba99d58abac581385de179787a82
NightlyD3V <TylerSpaulding95@gmail.com>
2019-12-31 01:18:58 +0000

    Adding blockstack auth

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
   componentDidMount() {
     //console.log('app client connected')
     socketio.emit('a client connected');
-    console.log(userSessionl.loadUserData().username);
+    console.log(userSession.loadUserData().username);
   }
 
   render() {
